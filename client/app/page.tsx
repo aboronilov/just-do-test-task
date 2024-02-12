08284940fb0ca0b0d8439d23f9bde4c76ba9583e
Anatoly Boronilov <boronilov@gmail.com>
2024-02-13 00:02:55 +0200

    preparation for readme

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+"use client"
+
+import { useAppSelector } from "@/redux/hooks";
+import { useRouter } from "next/navigation";
+
 
 export default function Page() {
-  return (
-    <main>
-      Hello
-    </main>
-  );
+  const {isAuthenticated} = useAppSelector(state => state.auth)
+  const router = useRouter()
+
+  if (isAuthenticated) {
+    router.push("/dashboard")
+  } else {
+    router.push("/auth/login")
+  }
 }
